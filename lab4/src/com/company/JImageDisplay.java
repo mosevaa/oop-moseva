509d937eca2c0b16442d5df1711f7c4b731b1f39
user <user@mail.ru>
2021-03-16 16:57:37 +0300

    changes lab 4

--- lab4/src/com/company/JImageDisplay.java
+++ lab4/src/com/company/JImageDisplay.java
@@ -8,8 +8,27 @@ public class JImageDisplay extends JComponent{
     private BufferedImage image;
 
     public JImageDisplay (int h, int w) {
-        image = new BufferedImage(h,w, 'TYPE_INT_RGB');
+        image = new BufferedImage(h,w, BufferedImage.TYPE_INT_RGB);
         Dimension size = new Dimension(h,w);
-        super.getPreferredSize(size);
+        super.setPreferredSize(size);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image,0,0,image.getWidth(),image.getHeight(),null);
+
+    }
+
+    public void clearImage() {
+        for (int i=0; i < image.getHeight(); i++ ) {
+            for (int j = 0; j < image.getWidth(); j ++) {
+                drawPixel(i,j,0);
+            }
+        }
+    }
+
+    public void drawPixel (int x, int y, int rgbColor) {
+        image.setRGB(x,y,rgbColor);
     }
 }
---
+++ lab4/src/com/company/Mandelbrot.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.awt.geom.Rectangle2D;
+
+public class Mandelbrot  extends FractalGenerator {
+    public static final int MAX_ITERATIONS = 2000;
+    @Override
+    public void getInitialRange(Rectangle2D.Double range) {
+        range.x = -2;
+        range.y = -1.5;
+        range.width = 3;
+        range.height = 3;
+    }
+
+    @Override
+    public int numIterations(double x, double y) {
+
+    }
+}
